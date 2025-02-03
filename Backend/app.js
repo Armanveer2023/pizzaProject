032fded628f5fecc56d5f73debf734ba77c8109c
Sarthak Bajaj <sarthak.bajaj@salescode.ai>
2025-02-03 21:01:56 +0530

    Project initiation

---
+++ Backend/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const AuthRouter = require('./routes/authRouter');
+const ProductRouter = require('./routes/productRouter');
+
+require('dotenv').config();
+const connectDb =require('./models/db');
+connectDb();
+const PORT = process.env.PORT || 8080;
+
+app.get('/ping', (req, res) => {
+    res.send('PONG');
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+app.use('/auth', AuthRouter);
+app.use('/products', ProductRouter);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${PORT}`)
+})
